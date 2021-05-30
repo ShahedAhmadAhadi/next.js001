1fef66aa7e1a5fb13900051d7d21c7f30b874113
Shahed Ahmad <shaheda.a16@gmail.com>
2021-05-30 13:31:09 +0430

    practices

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 export default function about() {
     return (
         <div>
             <h1>Hello, world</h1>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,7 +12,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      
+
       <main className={styles.main}>
+      <Link href="/about">
+        <a>about</a>
+      </Link>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
