93c314f6b4b0d2d7dec040ee45e2bd3a58527062
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-20 14:05:15 +0430

    docker file

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,29 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function about() {
+
+    const [values, setValues] = useState({
+        name: ''
+    })
+
+    const handleChange = (e) => {
+        const {value, name} = e.target
+        setValues({...values, [name]:value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(values)
+    }
     return (
         <div>
             <h1>Hello, world</h1>
+            <form onSubmit={handleSubmit}>
+
+                <input type="text" name="name" value={values.name} onChange={handleChange} />
+                <input type="submit" value="save"/>
+            </form>
             <Link href="/">
                 <a>Home</a>
             </Link>
